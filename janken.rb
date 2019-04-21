523b3c5b9fffd8cfe8f91491dc0083a9aa04efa6
ashitani0803 <ashitani0803@gmail.com>
2019-04-21 15:41:53 +0900

    [Update] じゃんけんプログラム

--- janken.rb
+++ janken.rb
@@ -4,7 +4,23 @@ puts '[0]:グー'
 puts '[1]:チョキ'
 puts '[2]:パー'
 a = gets.to_i
+if a == 0
+	print 'あなたの手:グー、'
+elsif a == 1
+	print 'あなたの手:チョキ、'
+else
+	print 'あなたの手:パー、'
+end
+
 b = rand(0..2)
+if b == 0
+	puts '相手の手:グー'
+elsif b == 1
+	puts '相手の手:チョキ'
+else
+	puts '相手の手:パー'
+end
+
 if a < 0 || a > 2
 	puts '入力された値が無効です'
 elsif a == b
